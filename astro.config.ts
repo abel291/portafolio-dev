706a3904096363b0c7d8f5c7e287f975e5860f7f
abel <abel291m@gmail.com>
2024-09-05 02:20:59 -0500

    created components and styles

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import vercelServerless from '@astrojs/vercel/serverless';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  integrations: [tailwind()],
   output: 'server',
   adapter: vercelServerless({
     imageService: true,
